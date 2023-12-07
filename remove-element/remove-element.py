b7c62c7a7f28cac71d433fe1dbd2fc8b8cbdedd0
seongmookLIM <152977641+seongmookLIM@users.noreply.github.com>
2023-12-07 09:45:04 +0900

    Time: 15 ms, Memory: 13.3 MB - LeetHub

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -5,24 +5,14 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        f_idx = 0
-        l_idx = len(nums)-1
-        count = 0
-        while f_idx<=l_idx:
-            if nums[f_idx] == val and nums[l_idx] != val:
-                temp = nums[f_idx]
-                nums[f_idx] = nums[l_idx]
-                nums[l_idx] = temp
-                f_idx+=1
-                l_idx-=1
-                count+=1
-            elif nums[f_idx] != val and nums[l_idx] == val:
-                f_idx+=1
-                l_idx-=1
-                count+=1
-            elif nums[f_idx] == val and nums[l_idx] == val:
-                l_idx-=1
-                count+=1
-            else: #nums[f_idx] != val and nums[l_idx] != val:
-                f_idx+=1
-        return(len(nums)-count)
+        left = 0
+        right = len(nums)-1
+        
+        while left<=right:
+            if nums[left] == val:
+                nums[left], nums[right] = nums[right], nums[left]
+                right -= 1
+            else:
+                left +=1
+        
+        return left
