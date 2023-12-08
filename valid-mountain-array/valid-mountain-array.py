f22b2f48471b6c3a66718afb1c18800b8da76a16
seongmookLIM <152977641+seongmookLIM@users.noreply.github.com>
2023-12-08 22:39:04 +0900

    Time: 153 ms, Memory: 14.6 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.py
+++ valid-mountain-array/valid-mountain-array.py
@@ -10,6 +10,6 @@ class Solution(object):
 
         while i+1<n and arr[i]<arr[i+1]:
             i+=1
-        while j-1>0 and arr[j-1]>arr[j]:
+        while j>0 and arr[j-1]>arr[j]:
             j-=1
         return(0<i == j<n-1)
