283b3b6c8382fddc4a93e3fc053f185270d69239
seongmookLIM <152977641+seongmookLIM@users.noreply.github.com>
2023-12-11 11:08:02 +0900

    Time: 144 ms, Memory: 15.7 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -5,9 +5,19 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        for i in range(0, len(nums)):
-            nums[i] = nums[i]*nums[i]
-        nums.sort()
+        answer = [0]*len(nums)
+
+        l = 0
+        r = len(nums)-1
+        while l<=r:
+            left = abs(nums[l])
+            right = abs(nums[r])
+            if left>right:
+                answer[r-l] = left*left
+                l+=1
+            else:
+                answer[r-l] = right*right
+                r-=1
             
-        return nums
+        return answer
         
